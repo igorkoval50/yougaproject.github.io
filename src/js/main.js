23240cde81c970a475bb5fa93305283a21c0348b
igor <ihor.koval.91@mail.ru>
2018-04-03 10:34:00 +0400

    Created responnsive menu

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+$(function() {
+	var link = $(".menu-link");
+	var linkActive = $(".menu-link_active");
+	var menu = $(".menu");
+	var nav = $(".menu a");
+	
+	link.on("click", function() {
+		link.toggleClass('menu-link_active');
+		menu.toggleClass('menu-active');
+	});
+	linkActive.on("click", function() {
+		link.removeClass('menu-link_active');
+	});
+	nav.on("click", function() {
+		menu.toggleClass('menu-active');
+		link.toggleClass('menu-link_active');
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
