ae5e535208e35e717b014138c4c5cfbfbf675772
igor <ihor.koval.91@mail.ru>
2018-04-07 17:40:52 +0400

    smooth croll add for menu

--- src/js/main.js
+++ src/js/main.js
@@ -37,6 +37,19 @@ $(function() {
         }
 
     });
+    ///smooth scrool////////////////////
+    $("#nav a").on("click", function(e) {
+
+        e.preventDefault();
+
+        var currentBlock = $(this).attr("href"),
+            currentBlockOffset = $(currentBlock).offset().top;
+
+        $("html, body").animate({
+            scrollTop: currentBlockOffset - 100
+        }, 500);
+
+    });
 
 
 
